a3e922655170c882a73465d2bafd643005303e2f
Cividati <rubens.cividati.teles@gmail.com>
2022-03-28 11:42:58 -0300

    feat: add database connection

---
+++ api/class/database.js
@@ -0,0 +1,46 @@
+// Initialize Cloud Firestore through Firebase
+import { initializeApp } from 'firebase/app'
+import { getFirestore } from 'firebase/firestore'
+import { doc, setDoc, getDoc } from 'firebase/firestore';
+
+export class database {
+    constructor(){
+      this.creds = {
+        apiKey: process.env.apiKey,
+        authDomain: process.env.authDomain,
+        projectId: process.env.projectId
+        }  
+        
+      initializeApp(this.creds)
+      this.db = getFirestore();
+    }
+
+    async create(collection, document, obj) {    
+      
+      try {
+        const docRef = await setDoc(doc(this.db, collection, document), obj);
+
+        console.log("Document written with ID: ", document);
+      } catch (e) {
+        console.error("Error adding document: ", e);
+      }
+    }
+
+    async read(collection, document) {
+        const docRef = doc(this.db, collection, document);
+        const docSnap = await getDoc(docRef);
+    
+        if (docSnap.exists()) {
+          console.log('Document data:', docSnap.data());
+          return docSnap.data()
+        } else {
+          // doc.data() will be undefined in this case
+          console.log('No such document!');
+        }
+    }
+
+    async update(collection, document, obj) {
+      this.create(collection, document, obj)
+    }
+}
+
